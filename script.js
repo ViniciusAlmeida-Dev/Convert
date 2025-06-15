76648891cb45793d1cf2409a96837f083746d806
ViniciusAlmeida-Dev <caio.almeida.dev@gmail.com>
2025-06-15 19:44:44 -0300

    FEAT: Obtendo a moeda selecionada

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+// Otendo os elementos do formulário.
+const form = document.querySelector('form');
 const amount = document.getElementById('amount');
+const currency = document.getElementById('currency');
 
 // anipulando o input amount para receber apenas números.
 amount.addEventListener("input", () => {
@@ -6,3 +9,8 @@ amount.addEventListener("input", () => {
   const hasCharacterRegex = /\D+/g
   amount.value = amount.value.replace(hasCharacterRegex, "")
 })
+
+// Capturando o evento de submit do formulário.
+form.onsubmit = (e) => {
+  e.preventDefault();
+}
