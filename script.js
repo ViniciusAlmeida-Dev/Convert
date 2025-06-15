3996838bf53953d8dcf600468a27023d86f524ab
ViniciusAlmeida-Dev <caio.almeida.dev@gmail.com>
2025-06-15 20:03:37 -0300

    FEAT:Identificando a moeda

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Obtendo a cotação da moeda (neste dia) 
+const USD = 5.54
+const EUR = 6.40
+const GBP = 7.52
+
 // Otendo os elementos do formulário.
 const form = document.querySelector('form');
 const amount = document.getElementById('amount');
@@ -13,4 +18,19 @@ amount.addEventListener("input", () => {
 // Capturando o evento de submit do formulário.
 form.onsubmit = (e) => {
   e.preventDefault();
+}
+
+// Função para converter a moeda.
+const convertCurrency = () => {
+  switch (currency.value) {
+    case 'USD':
+      convertCurrency(amount.value, USD, 'US$')
+      break;
+    case 'EUR':
+      convertCurrency(amount.value, EUR, '€')
+      break;
+    case 'GBP':
+      convertCurrency(amount.value, GBP, '£')
+      break;
+  }
 }
