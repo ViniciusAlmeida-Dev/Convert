208e94261fa2b97f46f776181a565da3e2b00cd2
ViniciusAlmeida-Dev <caio.almeida.dev@gmail.com>
2025-06-15 18:48:51 -0300

    FEAT: Obtendo o valor digitado

---
+++ script.js
@@ -0,0 +1,6 @@
+const amount = document.getElementById('amount');
+
+// anipulando o input amount para receber apenas números.
+amount.addEventListener("input", () => {
+  console.log(amount.value)
+})
